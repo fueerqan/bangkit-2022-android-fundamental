2c681e26a1f90c440071bee9f561e8d4983b1a6b
Muhammad Furqan <furqan@tokopedia.com>
2022-03-06 15:40:46 +0700

    Explicit Intent with Extras

--- app/src/main/java/com/furqan/androidfundamental/intent/FirstActivity.kt
+++ app/src/main/java/com/furqan/androidfundamental/intent/FirstActivity.kt
@@ -1,12 +1,35 @@
 package com.furqan.androidfundamental.intent
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import com.furqan.androidfundamental.R
 
 class FirstActivity : AppCompatActivity() {
+
+    private lateinit var etTitle: AppCompatEditText
+    private lateinit var etSubtitle: AppCompatEditText
+    private lateinit var etDescription: AppCompatEditText
+    private lateinit var btnGoToSecond: AppCompatButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        etTitle = findViewById(R.id.et_title)
+        etSubtitle = findViewById(R.id.et_subtitle)
+        etDescription = findViewById(R.id.et_description)
+        btnGoToSecond = findViewById(R.id.btn_go_to_second)
+
+        btnGoToSecond.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra(SecondActivity.EXTRA_TITLE, etTitle.text.toString())
+            intent.putExtra(SecondActivity.EXTRA_SUBTITLE, etSubtitle.text.toString())
+            intent.putExtra(SecondActivity.EXTRA_DESCRIPTION, etDescription.text.toString())
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/furqan/androidfundamental/intent/SecondActivity.kt
+++ app/src/main/java/com/furqan/androidfundamental/intent/SecondActivity.kt
@@ -2,11 +2,33 @@ package com.furqan.androidfundamental.intent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatTextView
 import com.furqan.androidfundamental.R
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var tvTitle: AppCompatTextView
+    private lateinit var tvSubtitle: AppCompatTextView
+    private lateinit var tvDescription: AppCompatTextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second2)
+        setContentView(R.layout.activity_second)
+
+        tvTitle = findViewById(R.id.tv_title)
+        tvSubtitle = findViewById(R.id.tv_subtitle)
+        tvDescription = findViewById(R.id.tv_description)
+
+        intent.let {
+            tvTitle.text = it.getStringExtra(EXTRA_TITLE)
+            tvSubtitle.text = it.getStringExtra(EXTRA_SUBTITLE)
+            tvDescription.text = it.getStringExtra(EXTRA_DESCRIPTION)
+        }
+
+    }
+
+    companion object {
+        const val EXTRA_TITLE = "EXTRA_TITLE"
+        const val EXTRA_SUBTITLE = "EXTRA_SUBTITLE"
+        const val EXTRA_DESCRIPTION = "EXTRA_DESCRIPTION"
     }
 }

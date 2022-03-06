7384bc5d4d8fc7f17874ecc076652c9dc229d1e5
Muhammad Furqan <furqan@tokopedia.com>
2022-03-06 22:03:53 +0700

    [4] Glide Template

--- app/src/main/java/com/furqan/androidfundamental/fragment/util/MessageHelper.kt
+++ app/src/main/java/com/furqan/androidfundamental/fragment/util/MessageHelper.kt
@@ -6,49 +6,49 @@ object MessageHelper {
     val LIST_MESSAGE = arrayListOf(
         MessageModel(
             senderId = 1,
-            imageUrl = "",
+            imageUrl = "https://media.istockphoto.com/photos/millennial-male-team-leader-organize-virtual-workshop-with-employees-picture-id1300972574?b=1&k=20&m=1300972574&s=170667a&w=0&h=2nBGC7tr0kWIU8zRQ3dMg-C5JLo9H2sNUuDjQ5mlYfo=",
             senderName = "Muhammad Furqan",
             senderLastMessage = "This is the last message sent by sender, I wrote it a bit long to show ellipsize at the end of the message. Ellipsize is the three dot at the end of the text."
         ),
         MessageModel(
             senderId = 2,
-            imageUrl = "",
+            imageUrl = "https://media.istockphoto.com/photos/millennial-male-team-leader-organize-virtual-workshop-with-employees-picture-id1300972574?b=1&k=20&m=1300972574&s=170667a&w=0&h=2nBGC7tr0kWIU8zRQ3dMg-C5JLo9H2sNUuDjQ5mlYfo=",
             senderName = "Student 1",
             senderLastMessage = "This is the last message sent by sender."
         ),
         MessageModel(
             senderId = 3,
-            imageUrl = "",
+            imageUrl = "https://img.freepik.com/free-photo/pleasant-looking-serious-man-stands-profile-has-confident-expression-wears-casual-white-t-shirt_273609-16959.jpg",
             senderName = "Student 2",
             senderLastMessage = "This is the last message sent by sender."
         ),
         MessageModel(
             senderId = 4,
-            imageUrl = "",
+            imageUrl = "https://img.freepik.com/free-photo/pleasant-looking-serious-man-stands-profile-has-confident-expression-wears-casual-white-t-shirt_273609-16959.jpg",
             senderName = "Student 3",
             senderLastMessage = "This is the last message sent by sender, I wrote it a bit long to show ellipsize at the end of the message. Ellipsize is the three dot at the end of the text."
         ),
         MessageModel(
             senderId = 5,
-            imageUrl = "",
+            imageUrl = "https://img.freepik.com/free-photo/pleasant-looking-serious-man-stands-profile-has-confident-expression-wears-casual-white-t-shirt_273609-16959.jpg",
             senderName = "Student 4",
             senderLastMessage = "This is the last message sent by sender, I wrote it a bit long to show ellipsize at the end of the message. Ellipsize is the three dot at the end of the text."
         ),
         MessageModel(
             senderId = 6,
-            imageUrl = "",
+            imageUrl = "https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",
             senderName = "Student 5",
             senderLastMessage = "This is the last message sent by sender."
         ),
         MessageModel(
             senderId = 7,
-            imageUrl = "",
+            imageUrl = "https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",
             senderName = "Student 6",
             senderLastMessage = "This is the last message sent by sender."
         ),
         MessageModel(
             senderId = 8,
-            imageUrl = "",
+            imageUrl = "https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",
             senderName = "Student 7",
             senderLastMessage = "This is the last message sent by sender, I wrote it a bit long to show ellipsize at the end of the message. Ellipsize is the three dot at the end of the text."
         )

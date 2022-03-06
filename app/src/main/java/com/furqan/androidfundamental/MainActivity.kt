eb65acc7ed6f4cdd9b9ce2369815068cc8fc74db
Muhammad Furqan <furqan@tokopedia.com>
2022-03-06 15:23:36 +0700

    [1] Intent Template

--- app/src/main/java/com/furqan/androidfundamental/MainActivity.kt
+++ app/src/main/java/com/furqan/androidfundamental/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.furqan.androidfundamental
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import com.furqan.androidfundamental.intent.FirstActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<AppCompatButton>(R.id.btn_go_to_intent).setOnClickListener {
+            startActivity(
+                Intent(this, FirstActivity::class.java)
+            )
+        }
     }
 }
---
+++ app/src/main/java/com/furqan/androidfundamental/intent/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.furqan.androidfundamental.intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.furqan.androidfundamental.R
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second2)
+    }
+}

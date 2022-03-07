af00bc0d3c63df7e15d08e78b1be82fc58c3c0a8
Muhammad Furqan <furqan@tokopedia.com>
2022-03-07 14:16:30 +0700

    Explicit Intent

--- app/src/main/java/com/furqan/androidfundamental/intent/FirstActivity.kt
+++ app/src/main/java/com/furqan/androidfundamental/intent/FirstActivity.kt
@@ -1,12 +1,59 @@
 package com.furqan.androidfundamental.intent
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import com.furqan.androidfundamental.R
 
 class FirstActivity : AppCompatActivity() {
+
+    /*
+        Shortcut :
+        Duplicate line => Mac (Cmd + D), Win (Ctrl + D)
+     */
+    private lateinit var etTitle: AppCompatEditText
+    private lateinit var etSubtitle: AppCompatEditText
+    private lateinit var etDescription: AppCompatEditText
+    private lateinit var btnGoToSecond: AppCompatButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        etTitle = findViewById(R.id.et_title)
+        etSubtitle = findViewById(R.id.et_subtitle)
+        etDescription = findViewById(R.id.et_description)
+        btnGoToSecond = findViewById(R.id.btn_go_to_second)
+
+        btnGoToSecond.setOnClickListener {
+            // Option 1
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra(SecondActivity.EXTRA_TITLE, etTitle.text.toString())
+            intent.putExtra(SecondActivity.EXTRA_SUBTITLE, etSubtitle.text.toString())
+            intent.putExtra(SecondActivity.EXTRA_DESCRIPTION, etDescription.text.toString())
+
+            startActivity(intent)
+
+//            // Option 2
+//            val intent = Intent(this, SecondActivity::class.java).apply {
+//                putExtra(SecondActivity.EXTRA_TITLE, etTitle.text.toString())
+//                putExtra(SecondActivity.EXTRA_SUBTITLE, etSubtitle.text.toString())
+//                putExtra(SecondActivity.EXTRA_DESCRIPTION, etDescription.text.toString())
+//            }
+//
+//            startActivity(intent)
+
+//            // Option 3
+//            startActivity(Intent(this, SecondActivity::class.java).apply {
+//                putExtra(SecondActivity.EXTRA_TITLE, etTitle.text.toString())
+//                putExtra(SecondActivity.EXTRA_SUBTITLE, etSubtitle.text.toString())
+//                putExtra(SecondActivity.EXTRA_DESCRIPTION, etDescription.text.toString())
+//            })
+
+//            // Option 4
+//            startActivity(Intent(this, SecondActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/furqan/androidfundamental/intent/SecondActivity.kt
+++ app/src/main/java/com/furqan/androidfundamental/intent/SecondActivity.kt
@@ -1,12 +1,32 @@
 package com.furqan.androidfundamental.intent
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatTextView
 import com.furqan.androidfundamental.R
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var tvTitle: AppCompatTextView
+    private lateinit var tvSubtitle: AppCompatTextView
+    private lateinit var tvDescription: AppCompatTextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second2)
+
+        tvTitle = findViewById(R.id.tv_title)
+        tvSubtitle = findViewById(R.id.tv_subtitle)
+        tvDescription = findViewById(R.id.tv_description)
+
+        tvTitle.text = intent.getStringExtra(EXTRA_TITLE)
+        tvSubtitle.text = intent.getStringExtra(EXTRA_SUBTITLE)
+        tvDescription.text = intent.getStringExtra(EXTRA_DESCRIPTION)
+
+    }
+
+    companion object {
+        const val EXTRA_TITLE = "EXTRA_TITLE"
+        const val EXTRA_SUBTITLE = "EXTRA_SUBTITLE"
+        const val EXTRA_DESCRIPTION = "EXTRA_DESCRIPTION"
     }
 }
